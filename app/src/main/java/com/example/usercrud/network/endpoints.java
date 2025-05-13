dd23168e63ff46ab7c77d16647063ebafbbea3f4
ASUS-TUFGaming <notcozygg@gmail.com>
2025-05-13 14:17:39 +0800

    updated for final - ruffaina

--- app/src/main/java/com/example/usercrud/network/endpoints.java
+++ app/src/main/java/com/example/usercrud/network/endpoints.java
@@ -1,11 +1,11 @@
 package com.example.usercrud.network;
 
 public class endpoints {
-    private static final String BASE_URL = "http://192.168.100.175/users/"; // Replace with your WAMP IP & folder
+    private static final String BASE_URL = "http://192.168.0.112/users/"; // Replace with your WAMP IP & folder
 
-    public static final String CREATE = BASE_URL + "create_user.php";
-    public static final String READ_ALL = BASE_URL + "fetch_users.php";
-    public static final String READ_ONE = BASE_URL + "get_user.php";
-    public static final String UPDATE = BASE_URL + "update_user.php";
-    public static final String DELETE = BASE_URL + "delete_user.php";
+    public static final String CREATE = BASE_URL + "user.php?action=create";
+    public static final String READ_ALL = BASE_URL + "user.php?action=read_all";
+    public static final String READ_ONE = BASE_URL + "user.php?action=read_one";
+    public static final String UPDATE = BASE_URL + "user.php?action=update";
+    public static final String DELETE = BASE_URL + "user.php?action=delete";
 }
